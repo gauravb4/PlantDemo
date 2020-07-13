0cf8a1cf4347ea3060257ab07e3c8ad68670330f
Gaurav <gbaney@usc.edu>
2020-07-12 19:00:21 -0700

    airsim added, need to merge in lighting

---
+++ Source/PlantDemo/PlantDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PlantDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLANTDEMO_API APlantDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
